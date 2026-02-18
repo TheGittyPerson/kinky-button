b8b4494de37a2b9d2000745448315bd0abf96716
Morpheus <wangmykj2001@gmail.com>
2026-02-18 20:54:50 +0900

    Move JS to dedicated directory and file

---
+++ assets/js/main.js
@@ -0,0 +1,66 @@
+const messages = [
+    "Good boy...",
+    "Ooh yes daddy...",
+    "🫦",
+    "That's right...",
+    "You like that, don't you?",
+    "Such a good boy...",
+    "I know you want it...",
+    "You can do better than that...",
+    "Ooohh...",
+    "So naughty of you...",
+    "Unhhh...",
+    "I like that you listen to me...",
+    "It's getting hot in here, isn't it?",
+    "Mmmhhhphhh...",
+    "I want you to be my good boy.",
+    "Keep going...",
+    "Good job....",
+    "Ohhhh my goooood....",
+    "That felt goooood...",
+    "Ooooohhh yes daddy, keep going...",
+    "You can do better than that, but I like this...",
+    "You impress me greatly...",
+    "Faster...",
+    "Harder...",
+    "I'm getting numb...",
+    "🥵",
+    "You're turning me on like a switch...",
+    "Amazing...",
+    "Don't stop now...",
+    "You make me feel so...",
+    "Is that all you got?",
+    "Give it to me...",
+    "Is that it? I know you can do more than that...",
+    "💦💦💦",
+    "Oooh, that's tight...",
+    "Keep cliking, Daddy...",
+    "Click me like you mean it...",
+    "This feels like heaven...",
+    "Oooohhh, I'm getting close...",
+    "Yes, right there...",
+    "I hope this is touchsrcreen...",
+    "I feel a lot more than a button today...",
+    "I'm almost there...",
+    "Don't stop, Daddy...",
+    "I want to feel your cursor all over me...",
+    "Mmphhh, Oohhh—just keep clicking Daddy...",
+    "What a good boy you are...",
+    "Oooohhh, YESSSS!!!! Ooohh my gooood...",
+    "Hold tight...",
+    "I can feel it...",
+    "Ohhh, what's happening...",
+    "I like this... I like this a lot...",
+    "Oooh, I wasn't expecting that...",
+    "Ooohhh, I wasn't ready for this...",
+];
+
+let clicks = 0;
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+    clicks++;
+    document.getElementById("click-count").textContent = `Clicks: ${clicks}`;
+    const randomMessage =
+        messages[Math.floor(Math.random() * messages.length)];
+    alert(randomMessage);
+});
